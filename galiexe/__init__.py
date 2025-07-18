5917fe6f98e46d3f913f8eb30a3b3fe8dd05abb6
abhayabikramshahi <abhayabikramshahiofficial@gmail.com>
2025-07-18 20:12:59 +0545

    changes in code

--- galiexe/__init__.py
+++ galiexe/__init__.py
@@ -1,3 +1,9 @@
-from abhaya.gali import galiexe
+from .core import galiexe
 
-print(galiexe())  # spits out a random Nepali gali
+if __name__ == "__main__":
+    print("\033[95m==============================\033[0m")
+    print("\033[94m  Welcome to Galiexe!  \033[0m")
+    print("\033[95m==============================\033[0m")
+    print("\nGet a random Nepali gali (slang) every time you run!\n")
+    print("\033[92mYour gali:\033[0m", galiexe())
+    print("\n\033[93mTip: Import galiexe and call galiexe() to get a random gali in your code!\033[0m")
--- galiexe/core.py
+++ galiexe/core.py
@@ -20,3 +20,24 @@ def galiexe():
         "Dhaago"
     ]
     return random.choice(gali_list)
+
+if __name__ == "__main__":
+    gali_list = [
+        "Machikne",
+        "Randi",
+        "Randa",
+        "Baal",
+        "Gadha",
+        "Khopdi",
+        "Kutu",
+        "Bhuddu",
+        "Harayo",
+        "Tarkari",
+        "Jhatkaa",
+        "Bhari",
+        "Khatti",
+        "Dhaago"
+    ]
+    print("All Gali List:")
+    for gali in gali_list:
+        print(gali)
